154b3d00cafc4d4e2e53512bd6c7e62cf5c1db2f
Charly Garcia <97969737+CHARLY-GARCIA19@users.noreply.github.com>
2022-01-30 06:34:03 -0600

    Update tarea1/main.cpp

--- tarea1/main.cpp
+++ tarea1/main.cpp
@@ -5,9 +5,9 @@ using namespace std;
 int main()
 {
     int n1, n2;
-    cout << "Introduce el primer nœmero para las operaciones matematicas: ";
+    cout << "Introduce el primer numero para las operaciones matematicas: ";
     cin >> num1;
-    cout << "Introduce el primer nœmero para las operaciones matematicas: ";
+    cout << "Introduce el primer numero para las operaciones matematicas: ";
     cin >> num2;
     cout << "La suma de los numeros es: " << n1 + n2 << endl;
     cout << "La resta de los numeros es: " << n1 - n2 << endl;
